6afba2e5eddef92715457d4942e7d7128eec60f1
mcfreak12 <jmft87@mail.umkc.edu>
2023-03-11 14:05:33 -0600

    read in files as strings

--- Project 2/program2.rkt
+++ Project 2/program2.rkt
@@ -1,2 +1,10 @@
 #lang racket
+(require 2htdp/batch-io)
 
+(define (process-files f1 f2)
+  (let
+      ([file-string1 (string-downcase(read-file f1))]
+       [file-string2 (string-downcase(read-file f2))])
+    (string-trim file-string1 #:repeat? #t)))
+    
+(process-files "Doyle.txt" "Lovecraft.txt")
