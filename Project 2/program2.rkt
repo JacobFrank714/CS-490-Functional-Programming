c633ae8f8b04da0f54c61256f0a17814beae7dd6
mcfreak12 <jacobfrank714@gmail.com>
2023-03-11 18:53:46 -0600

    comparison good need final decision

--- Project 2/program2.rkt
+++ Project 2/program2.rkt
@@ -18,21 +18,42 @@
   (define (iter so-far to-go count)
     (if (empty? to-go)
     (list so-far count)
-    (if (hash-ref so-far (first to-go) #f)
+    (if (hash-has-key? so-far (first to-go))
     (iter (hash-set so-far (first to-go) (+ (hash-ref so-far (first to-go)) 1)) (rest to-go) (+ count 1))
     (iter (hash-set so-far (first to-go) 1) (rest to-go) (+ count 1)))))
   (let ([table (hash)])
     (iter table lst 0)))
 
-(define (normalize table lst count)
-    (if (empty? lst)
+(define (normalize table count)
+  (define (iter table lst count)
+  (if (empty? lst)
     table
-    (normalize (hash-set table (first lst) (* (log (/ (hash-ref table (first lst)) count) 10) -1)) (rest lst) count)))
+    (iter (hash-set table (first lst) (* (log (/ (hash-ref table (first lst)) count) 10) -1)) (rest lst) count))
+    )
+  (let ([lst (hash-keys table)])
+  (iter table lst count)))
+    
 
-(define lovecraft (process-file "Lovecraft.txt"))
-(define Doyle (process-file "Doyle.txt"))
+(define (compare text-1 text-2)
+  (define (iter text-1 text-2 keys compared)
+    (if (empty? keys)
+      compared
+      (if (hash-has-key? text-1)
+        (hash-set compared (first keys) (abs(- (hash-ref text-1 (first keys)) (hash-ref text-2 (first keys)))))
+        (iter text-1 text-2 (rest keys) compared))))
+  (let ([keys (hash-keys text-2)]
+        [compared (hash-copy text-2)])
+    (iter text-1 text-2 keys compared)))
 
-(define pre-normal-lovecraft(make-reference-hash lovecraft))
-(define pre-normal-doyle(make-reference-hash Doyle))
+(define (decision from-lovecraft from-doyle)
+  from-doyle)
 
-(normalize (first pre-normal-doyle) Doyle (second pre-normal-doyle))
+(define pre-normal-lovecraft(make-reference-hash (process-file "Lovecraft.txt")))
+(define pre-normal-doyle(make-reference-hash (process-file "Doyle.txt")))
+(define pre-normal-mystery1(make-reference-hash (process-file "mystery1.txt")))
+(define pre-normal-mystery2(make-reference-hash (process-file "mystery2.txt")))
+
+(define normal-doyle (normalize (first pre-normal-doyle) (second pre-normal-doyle)))
+(define normal-lovecraft (normalize (first pre-normal-lovecraft) (second pre-normal-lovecraft)))
+(define normal-mystery1 (normalize (first pre-normal-mystery1) (second pre-normal-mystery1)))
+(define normal-mystery2 (normalize (first pre-normal-mystery2) (second pre-normal-mystery2)))
