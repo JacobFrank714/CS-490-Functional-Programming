fc5d7c8f23bf6659e11475b33d0f8dfb6a59aedf
mcfreak12 <jmft87@mail.umkc.edu>
2023-04-09 22:31:31 -0500

    boiler plate work

--- Project 3/Program3.rkt
+++ Project 3/Program3.rkt
@@ -1,1 +1,60 @@
 #lang racket
+(require data/maybe)
+(require data/either)
+(require typed-stack)
+
+(define (CLR)
+    (empty-stack))
+
+(define (SHOW stack)
+    (displayln (in-stack stack)))
+
+(define (TOP stack)
+    (displayln (top stack)))
+
+(define (SIZ stack)
+    (stack-length stack))
+
+(define (DUP stack)
+    (push stack (top stack)))
+
+(define (ADD stack)
+    (let
+    ([x (pop! stack)]
+    [y (pop! stack)])
+    (push stack (+ x y))))
+
+(define (SUB stack)
+    (let
+    ([x (pop! stack)]
+    [y (pop! stack)])
+    (push stack (- x y))))
+
+(define (MUL stack)
+    (let
+    ([x (pop! stack)]
+    [y (pop! stack)])
+    (push stack (* x y))))
+
+(define (DIV stack)
+    (let
+    ([x (pop! stack)]
+    [y (pop! stack)])
+    (push stack (/ x y))))
+
+;;; (define (END stack)
+;;;     )
+
+(let loop ()
+    (display "Input: ")
+    (define a (read-line (current-input-port) 'any))
+    (define (iter stack to-do)
+        (if (empty? to-do)
+            stack
+            (iter (push stack (first to-do)) (rest to-do))))
+    (let ([in (reverse (string-split a " "))])
+        (define stack (iter (empty-stack) in))
+        (displayln (in-stack stack)))
+    
+    
+    (loop))
